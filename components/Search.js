f61b1cea8a4456d7957d9e982cf0f69f6e086815
Przemyslaw Cygan <cyganprzemyslaw@gmail.com>
2018-10-03 21:31:19 +0200

    Change index.html, Change script.js, Change package.json, Change App and Search Components

--- components/Search.js
+++ components/Search.js
@@ -1,4 +1,23 @@
 Search = React.createClass({
+    getInitialState() {
+      return {
+        searchingText: ''
+      };
+    },
+    handleChange: function(event) {
+        var searchingText = event.target.value;
+        this.setState({searchingText: searchingText});
+
+        if (searchingText.length > 2) {
+          this.props.onSearch(searchingText);
+        }
+      },
+
+      handleKeyUp: function(event) {
+        if (event.keyCode === 13) {
+          this.props.onSearch(this.state.searchingText);
+        }
+    },
     render: function() {
         var styles = {
             fontSize: '1.5em',
@@ -9,6 +28,7 @@ Search = React.createClass({
     return <input
              type="text"
              onChange={this.handleChange}
+             onKeyUp={this.handleKeyUp}
              placeholder="Tutaj wpisz wyszukiwaną frazę"
              style={styles}
              value={this.state.searchTerm}
